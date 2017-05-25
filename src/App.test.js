dde2f54b08b9d7915474b45c698260e22a2e0ef6
Aleksi Mustonen <mustonen.aleksi@kolumbus.fi>
2017-05-25 22:19:47 +0300

    written more tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Game from './TicTacToe/Game.js'
+import Board from './TicTacToe/Board.js'
 import Platform from './RockPaperScissor/Platform.js'
+import RockPaperScissor from './RockPaperScissor/RockPaperScissor.js'
 
 var TestUtils = require('react-dom/test-utils');
 
@@ -11,7 +13,9 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
 });
 
-it("Platform renders correct h1", function () {
+
+
+it("Rock paper scissors Platform renders correct h1", function () {
     var component = TestUtils.renderIntoDocument(
         < Platform/>
     );
@@ -24,7 +28,51 @@ it("Platform renders correct h1", function () {
         .toEqual("Play rock-paper-scissors");
 });
 
-it ("Game renders correct status at start", function () {
+it("Shows right score when no rounds played", function() {
+  var component = TestUtils.renderIntoDocument(
+      < RockPaperScissor/>
+  );
+
+  var h2 = TestUtils.findRenderedDOMComponentWithTag(
+    component, 'h2'
+  );
+
+
+  expect(ReactDOM.findDOMNode(h2).textContent)
+      .toEqual("You vs. AI 0 - 0");
+
+});
+
+it("Shows right amounts of round played at start", function() {
+  var component = TestUtils.renderIntoDocument(
+      < RockPaperScissor/>
+  );
+
+  var h3 = TestUtils.findRenderedDOMComponentWithTag(
+    component, 'h3'
+  );
+
+
+  expect(ReactDOM.findDOMNode(h3).textContent)
+      .toEqual("No rounds played.");
+});
+
+it("Shows correctly chosen element", function() {
+  var component = TestUtils.renderIntoDocument(
+      < RockPaperScissor/>
+  );
+
+  component.handleClick("ROCK");
+
+  var h3 = TestUtils.findRenderedDOMComponentWithTag(
+    component, 'h3'
+  );
+
+  expect(ReactDOM.findDOMNode(h3).textContent)
+      .toContain("You selected ROCK");
+});
+
+it ("TicTacToe renders correct status at start", function () {
     var component = TestUtils.renderIntoDocument(
         < Game/>
     );
@@ -35,5 +83,22 @@ it ("Game renders correct status at start", function () {
 
     expect(ReactDOM.findDOMNode(h1).textContent)
         .toEqual("Next player: X");
+
+});
+
+it ("TicTacToe shows 0:s turn after first move", function() {
+  var component = TestUtils.renderIntoDocument(
+      < Game/>
+  );
+
+  component.handleClick(1);
+
+  var h1 = TestUtils.findRenderedDOMComponentWithTag(
+    component, 'h1'
+  );
+
+  expect(ReactDOM.findDOMNode(h1).textContent)
+      .toEqual("Next player: O");
+
 
 });
